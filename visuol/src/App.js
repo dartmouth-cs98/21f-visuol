21d8e43236e1761e43c1db931b82fcd17403912a
Raymond <raymond.g.chen.22@dartmouth.edu>
2021-10-24 16:22:14 -0400

    Added router navigation to app

--- visuol/src/App.js
+++ visuol/src/App.js
@@ -1,12 +1,31 @@
+//Css is used from https://ant.design/components/layout/
 import './App.css';
+import 'antd/dist/antd.css';
 import Login from './Login';
-import Registration from './Registration'
+import Registration from './Registration';
+import SideMenu from './Menu';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
 function App() {
   return (
-    <div className="App">
-      <Login />
-      <Registration />
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Sider width={256} className="site-layout-background">
+          <SideMenu />
+        </Sider>
+        <Layout style={{padding: '48px 24px 24px'}}>
+            <Switch>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <Route path="/register">
+                <Registration />
+              </Route>
+            </Switch>
+        </Layout>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
