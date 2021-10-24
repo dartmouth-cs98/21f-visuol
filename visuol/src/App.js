47911597cbee8b0e28859953836af2a675535426
Raymond Chen <raymond.g.chen.22@dartmouth.edu>
2021-10-24 09:17:19 -0400

    Added registration form (#46)
    * Added registration form
    
    * Added new line to end of files

--- visuol/src/App.js
+++ visuol/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Login from './Login';
+import Registration from './Registration'
 function App() {
   return (
     <div className="App">
       <Login />
+      <Registration />
     </div>
   );
 }
--- visuol/src/Registration.js
+++ visuol/src/Registration.js
@@ -1,16 +1,32 @@
 //Credit to https://ant.design/components/form/
 //Component for logging in
-import { Form, Input, Button} from 'antd';
+import { Form, Input, Button, Switch} from 'antd';
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
-class Login extends Component {
+class Registration extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      displayCompanyForm : false,
+    }
+  }
+
+  
   //OnFinish dictates what happens when submitting a form object
   onFinish = (values) => {
     console.log('Success:', values);
+    console.log('Company?', this.state.displayCompanyForm);
   };
 
-   onFinishFailed = (errorInfo) => {
+  onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
+  };
+
+  onClickSwitch = () => {
+    this.setState((state) => {
+      return {displayCompanyForm: !state.displayCompanyForm};
+    });
   };
 
   render() {
@@ -31,12 +47,25 @@ class Login extends Component {
         autoComplete="off"
       >
         <Form.Item
+          label="Name"
+          name="name"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your full name (First and last)',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
           label="Username"
           name="username"
           rules={[
             {
               required: true,
-              message: 'Please input your username!',
+              message: 'Please input your desired username!',
             },
           ]}
         >
@@ -56,6 +85,23 @@ class Login extends Component {
           <Input.Password />
         </Form.Item>
 
+        <Form.Item label="Company Account" valuePropName="checked">
+          <Switch onClick={this.onClickSwitch}/>
+        </Form.Item>
+
+        <Form.Item
+          label="Company Name"
+          name="company"
+          rules={[
+            {
+              required: this.state.displayCompanyForm,
+              message: 'Please input your company name',
+            },
+          ]}
+        >
+          <Input disabled={!this.state.displayCompanyForm} />
+        </Form.Item>
+
         <Form.Item
           wrapperCol={{
             offset: 8,
@@ -71,4 +117,4 @@ class Login extends Component {
   }
 };
 
-export default Login;
+export default Registration;
