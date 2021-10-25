8681661cf042a0f8ec123fa9fb79704f1e6d891d
jamesversch <james.r.verschleiser.22@dartmouth.edu>
2021-10-25 14:17:20 -0400

    added url

--- visuol/src/OfferAPI.js
+++ visuol/src/OfferAPI.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
+BASE_URL = 'http://localhost:5000/'
+
 // Calls the backend api and receives the offer
 function getOffer(offer_id) {
-    axios.get('' + 'api_v1/create_offer/' + offer_id)
+    axios.get(BASE_URL + 'api_v1/create_offer/' + offer_id)
     .then(response => {
         return response
     })
@@ -10,7 +12,7 @@ function getOffer(offer_id) {
 
 // Calls the backend api to post the offer in the database
 function postOffer(offer) {
-    axios.post('' + '/api_v1/create_offer', offer)
+    axios.post(BASE_URL + '/api_v1/create_offer', offer)
     .then(response => {
         return response
     })
