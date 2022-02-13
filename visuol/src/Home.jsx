d5e4d71b7305ac643ad6a3d1865d503d73651857
Raymond <raymond.g.chen.22@dartmouth.edu>
2022-02-13 13:06:36 -0500

    Updated home page

--- visuol/src/Home.jsx
+++ visuol/src/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable react/button-has-type */
 import React, { PureComponent } from 'react';
 // Referenced from https://codesandbox.io/s/1l1xu?file=/index.js:190-278
@@ -22,6 +23,15 @@ class Home extends PureComponent {
             <p>Make an account using register, then log in.</p>
             <p>Recruiters can send offers to candidates as well as view outstanding offers.</p>
             <p>Candidates can interact with their offers to see how compensation grows.</p>
+          </Panel>
+          <Panel header='What can I do with my offers?' key='2'>
+            <p>Using the sliders, you can view how your compensation would grow over time given different growth rates. You can also see how much you would have saved in accumulations over time.</p>
+          </Panel>
+          <Panel header='Why are there no local taxes?' key='3'>
+            <p>Because there are very few cities that charge local income taxes, and they make up such a small percentage of your total compensation, we deemed it unneccesary to include</p>
+          </Panel>
+          <Panel header='What does comparing offers do?' key='4'>
+            <p>You can display two offers side by side to compare how savings or compensation differ</p>
           </Panel>
         </Collapse>
         <Countdown title='Days until launch' value={deadline} format='DD:HH:mm:ss:SSS' style={{ paddingBottom: '5%' }} />
