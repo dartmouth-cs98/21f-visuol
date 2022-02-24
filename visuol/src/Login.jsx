47d881be766ce72e2e6abfd73ea90d48db866911
jamesversch17 <james.r.verschleiser.22@dartmouth.edu>
2022-02-24 14:11:06 -0500

    got rid of test

--- visuol/src/Login.jsx
+++ visuol/src/Login.jsx
@@ -65,7 +65,6 @@ class Login extends Component {
          onFinishFailed={this.onFinishFailed}
          autoComplete='off'
        >
-         <h1>Test</h1>
          <Form.Item
            label='Email'
            name='email'
